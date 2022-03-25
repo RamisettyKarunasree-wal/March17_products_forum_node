2da45768fabb48f842f78041b7193321fea2b779
RamisettyKarunasree-wal <karuna.r@westagilelabs.com>
2022-03-25 14:09:01 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-import Products from './Products';
-import Forums from './Forums';
+import Photos from './Photos';
 
 function App() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <div className="App">
-      <BrowserRouter>
-        <NavLink activeClassName="active" className="links" to="/products">
-          Products
-        </NavLink>
-        <NavLink activeClassName="active" className="links" to="/forums">
-          Forums
-        </NavLink>
-        <Routes>
-          <Route path="/products" element={<Products />} />
-          <Route path="/forums" element={<Forums />} />
-        </Routes>
-      </BrowserRouter>
+      <Photos />
     </div>
   );
 }
